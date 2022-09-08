b7238aedc571455c3b30552e501db62a4ab7dd22
TohidEq <TohidEq@gmail.com>
2022-09-09 04:14:41 +0430

    Make it ready

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    "*.html",
+  ],
+  theme: {
+    extend: {
+      
+    },
+    colors: {
+      'darck-c-2': "#343434",
+      'darck-c-1' : "#8E8B82",
+      'white-c': "#F3F3F3",
+      'dead-c': "#DCDCD9",
+      'hover-c': "#E9DCBE",
+      'clicked-c': "#F1D28B"
+
+
+    }
+  },
+  plugins: [],
+}
