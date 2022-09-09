925f33198bc5718de739fd9b509691eec2a78ce6
TohidEq <TohidEq@gmail.com>
2022-09-09 05:29:59 +0430

    Design Calculator form

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,10 @@ module.exports = {
       
     },
     colors: {
+      'darck-c-3': "#0b0d0b",
       'darck-c-2': "#343434",
       'darck-c-1' : "#8E8B82",
+      'red-c': "#d62d2d",
       'white-c': "#F3F3F3",
       'dead-c': "#DCDCD9",
       'hover-c': "#E9DCBE",
